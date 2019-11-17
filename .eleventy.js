9899cf75ea0571c32316dcc07799b94cf243b2ab
Jess <feralfp@gmail.com>
2019-11-17 21:10:27 +0800

    add rss feed -test

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-module.exports = function(config) {
+
+module.exports = function (config) {
+
+  config.addPlugin(pluginRss);
 
   // A useful way to reference the context we are runing eleventy in
   let env = process.env.ELEVENTY_ENV;
@@ -11,7 +15,7 @@ module.exports = function(config) {
     html: true
   };
   let markdownLib = markdownIt(options).use(markdownItEmoji);
-  
+
   // eleventyConfig.setLibrary("md", markdownLib);
   config.setLibrary("md", markdownLib);
 
@@ -19,8 +23,8 @@ module.exports = function(config) {
   config.addLayoutAlias('default', 'layouts/base.njk');
 
   // Add some utility filters
-  config.addFilter("squash", require("./src/utils/filters/squash.js") );
-  config.addFilter("dateDisplay", require("./src/utils/filters/date.js") );
+  config.addFilter("squash", require("./src/utils/filters/squash.js"));
+  config.addFilter("dateDisplay", require("./src/utils/filters/date.js"));
 
 
   // add support for syntax highlighting
@@ -30,31 +34,32 @@ module.exports = function(config) {
   config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
 
   // compress and combine js files
-  config.addFilter("jsmin", function(code) {
+  config.addFilter("jsmin", function (code) {
     const UglifyJS = require("uglify-js");
     let minified = UglifyJS.minify(code);
-      if( minified.error ) {
-          console.log("UglifyJS error: ", minified.error);
-          return code;
-      }
-      return minified.code;
+    if (minified.error) {
+      console.log("UglifyJS error: ", minified.error);
+      return code;
+    }
+    return minified.code;
   });
+
 
 
   // pass some assets right through
   config.addPassthroughCopy("./src/site/images");
 
   // make the seed target act like prod
-  env = (env=="seed") ? "prod" : env;
+  env = (env == "seed") ? "prod" : env;
   return {
     dir: {
       input: "src/site",
       output: "dist",
       data: `_data/${env}`
     },
-    templateFormats : ["njk", "md", "11ty.js"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
+    templateFormats: ["njk", "md", "11ty.js"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
     passthroughFileCopy: true
   };
 
